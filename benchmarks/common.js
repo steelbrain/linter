382a0646951dca66788e9e5b919246011f782839
steelbrain <de.steelbrain@gmail.com>
2016-02-11 07:42:53 +0000

    :arrow_up: Upgrade benchmarks to use the latest message-registry API

--- benchmarks/common.js
+++ benchmarks/common.js
@@ -2,15 +2,6 @@
 
 import {getMessage} from '../spec/common'
 
-export function timeNow() {
-  return process.hrtime()
-}
-
-export function timeDiff(last) {
-  const diff = process.hrtime(last)
-  return Math.round((diff[0] + (diff[1] * 1e-9)) * 1000)
-}
-
 export function getMessages(count) {
   const messages = []
   for (let i = 0; i < count; ++i) {
--- benchmarks/message-registry.js
+++ benchmarks/message-registry.js
@@ -1,41 +1,22 @@
 'use babel'
 
-import MessageRegistry from '../lib/message-registry'
+import {MessageRegistry} from '../lib/message-registry'
 import {getLinter, getMessage} from '../spec/common'
 import {timeNow, timeDiff, getMessages} from './common'
 
 const messageRegistry = new MessageRegistry()
-messageRegistry.active = false // Disable the auto-updater
-messageRegistry.shouldRefresh = false // Disable the auto-updater
+messageRegistry.debouncedUpdate = function() {}
 const linter = getLinter()
 
-let time = null
-let messages = null
-
 function benchmarkRegistry(i) {
+  let timeKey = 'iteration #' + i
   messages = getMessages(5000)
-  time = timeNow()
-
-  messageRegistry.set({linter, messages})
-  if (messageRegistry.updatePublic) {
-    messageRegistry.updatePublic()
-  } else if (messageRegistry.processQueue) {
-    messageRegistry.processQueue()
-  }
-  const diff = timeDiff(time)
-  console.log('iteration #', i,'took', diff, 'ms')
-  return diff
+  console.time(timeKey)
+  messageRegistry.set({linter, messages, buffer: null})
+  messageRegistry.update()
+  console.timeEnd(timeKey)
 }
 
-let promise = Promise.resolve()
-let sum = 0
-let count = 1000
-
-for (let i = 0; i < count; ++i) {
-  promise = promise.then(function() {
-    sum += benchmarkRegistry(i)
-  })
+for (let i = 0; i < 50; ++i) {
+  benchmarkRegistry(i)
 }
-promise.then(function() {
-  console.log('average', sum / count)
-})
