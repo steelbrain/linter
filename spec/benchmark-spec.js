2f6841ec4d8992fff3bdb977a091e72060a38a5b
steelbrain <de.steelbrain@gmail.com>
2016-06-02 08:30:24 -0700

    :fire: Remove benchmarks for now

--- spec/benchmark-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel'
-
-describe('Benchmarks', function() {
-  describe('Message Registry', function() {
-    it('is fast', function() {
-      require('../benchmarks/message-registry')()
-    })
-  })
-})
