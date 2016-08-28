3e771ce7cf9fea3fe788565d9b6dc318e8c0d2d9
Steel Brain <i@steelbrain.me>
2016-08-28 08:48:15 -0700

    :white_check_mark: Add specs for EditorRegistry

---
+++ spec/editor-registry-spec.js
@@ -0,0 +1,87 @@
+/* @flow */
+
+import { it, beforeEach } from 'jasmine-fix'
+import EditorRegistry from '../lib/editor-registry'
+
+describe('EditorRegistry', function() {
+  let editorRegistry
+
+  beforeEach(async function() {
+    await atom.workspace.open(__filename)
+    editorRegistry = new EditorRegistry()
+  })
+  afterEach(async function() {
+    atom.workspace.destroyActivePane()
+    editorRegistry.dispose()
+  })
+
+  describe('::constructor', function() {
+    it('is a saint', function() {
+      expect(function() {
+        return new EditorRegistry()
+      }).not.toThrow()
+    })
+  })
+
+  describe('::activate && ::createFromTextEditor', function() {
+    it('adds current open editors to registry', function() {
+      expect(editorRegistry.editorLinters.size).toBe(0)
+      editorRegistry.activate()
+      expect(editorRegistry.editorLinters.size).toBe(1)
+    })
+    it('adds editors as they are opened', async function() {
+      expect(editorRegistry.editorLinters.size).toBe(0)
+      editorRegistry.activate()
+      expect(editorRegistry.editorLinters.size).toBe(1)
+      await atom.workspace.open()
+      expect(editorRegistry.editorLinters.size).toBe(2)
+    })
+    it('removes the editor as it is closed', async function() {
+      expect(editorRegistry.editorLinters.size).toBe(0)
+      editorRegistry.activate()
+      expect(editorRegistry.editorLinters.size).toBe(1)
+      await atom.workspace.open()
+      expect(editorRegistry.editorLinters.size).toBe(2)
+      atom.workspace.destroyActivePaneItem()
+      expect(editorRegistry.editorLinters.size).toBe(1)
+      atom.workspace.destroyActivePane()
+      expect(editorRegistry.editorLinters.size).toBe(0)
+    })
+  })
+  describe('::observe', function() {
+    it('calls with current editors and updates as new are opened', async function() {
+      let timesCalled = 0
+      editorRegistry.observe(function() {
+        timesCalled++
+      })
+      expect(timesCalled).toBe(0)
+      editorRegistry.activate()
+      expect(timesCalled).toBe(1)
+      await atom.workspace.open()
+      expect(timesCalled).toBe(2)
+    })
+  })
+  describe('::dispose', function() {
+    it('disposes all the editors on dispose', async function() {
+      let timesDisposed = 0
+      editorRegistry.observe(function(editorLinter) {
+        editorLinter.onDidDestroy(function() {
+          timesDisposed++
+        })
+      })
+      expect(timesDisposed).toBe(0)
+      editorRegistry.activate()
+      expect(timesDisposed).toBe(0)
+      atom.workspace.destroyActivePaneItem()
+      expect(timesDisposed).toBe(1)
+      await atom.workspace.open()
+      expect(timesDisposed).toBe(1)
+      atom.workspace.destroyActivePaneItem()
+      expect(timesDisposed).toBe(2)
+      await atom.workspace.open()
+      await atom.workspace.open()
+      editorRegistry.dispose()
+      expect(timesDisposed).toBe(4)
+    })
+  })
+})
