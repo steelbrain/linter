a52f556907455aae3ad6e103355fbf7cd08e731b
Steel Brain <i@steelbrain.me>
2017-03-01 12:19:13 +0500

    :new: Add specs for new Commands behavior

--- spec/commands-spec.js
+++ spec/commands-spec.js
@@ -17,16 +17,28 @@ describe('Linter Commands', function() {
   it('properly notifies its listeners of command execution', async function() {
     let lintCalled = 0
     let toggleCalled = 0
+    let showProvidersCalled = 0
 
     commands.onShouldLint(() => ++lintCalled)
     commands.onShouldToggleActiveEditor(() => ++toggleCalled)
+    commands.onShouldShowLinterProviders(() => ++showProvidersCalled)
 
     await atom.workspace.open(Path.join(__dirname, 'fixtures', 'file.txt'))
     const textEditor = atom.views.getView(atom.workspace.getActiveTextEditor())
+    expect(lintCalled).toBe(0)
+    expect(toggleCalled).toBe(0)
+    expect(showProvidersCalled).toBe(0)
     atom.commands.dispatch(textEditor, 'linter:lint')
+    expect(lintCalled).toBe(1)
+    expect(toggleCalled).toBe(0)
+    expect(showProvidersCalled).toBe(0)
     atom.commands.dispatch(textEditor, 'linter:toggle-active-editor')
-
     expect(lintCalled).toBe(1)
     expect(toggleCalled).toBe(1)
+    expect(showProvidersCalled).toBe(0)
+    atom.commands.dispatch(textEditor, 'linter:show-linter-providers')
+    expect(lintCalled).toBe(1)
+    expect(toggleCalled).toBe(1)
+    expect(showProvidersCalled).toBe(1)
   })
 })
