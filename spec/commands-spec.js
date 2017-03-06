c210310c98d7de776805729927087408523f2055
Steel Brain <i@steelbrain.me>
2017-03-06 23:10:28 +0500

    :minibus: Rename linter:show-linter-providers to linter:debug

--- spec/commands-spec.js
+++ spec/commands-spec.js
@@ -16,29 +16,29 @@ describe('Linter Commands', function() {
   })
   it('properly notifies its listeners of command execution', async function() {
     let lintCalled = 0
+    let debugCalled = 0
     let toggleCalled = 0
-    let showProvidersCalled = 0
 
     commands.onShouldLint(() => ++lintCalled)
     commands.onShouldToggleActiveEditor(() => ++toggleCalled)
-    commands.onShouldShowLinterProviders(() => ++showProvidersCalled)
+    commands.onShouldDebug(() => ++debugCalled)
 
     await atom.workspace.open(Path.join(__dirname, 'fixtures', 'file.txt'))
     const textEditor = atom.views.getView(atom.workspace.getActiveTextEditor())
     expect(lintCalled).toBe(0)
+    expect(debugCalled).toBe(0)
     expect(toggleCalled).toBe(0)
-    expect(showProvidersCalled).toBe(0)
     atom.commands.dispatch(textEditor, 'linter:lint')
     expect(lintCalled).toBe(1)
+    expect(debugCalled).toBe(0)
     expect(toggleCalled).toBe(0)
-    expect(showProvidersCalled).toBe(0)
     atom.commands.dispatch(textEditor, 'linter:toggle-active-editor')
     expect(lintCalled).toBe(1)
     expect(toggleCalled).toBe(1)
-    expect(showProvidersCalled).toBe(0)
-    atom.commands.dispatch(textEditor, 'linter:show-linter-providers')
+    expect(debugCalled).toBe(0)
+    atom.commands.dispatch(textEditor, 'linter:debug')
     expect(lintCalled).toBe(1)
+    expect(debugCalled).toBe(1)
     expect(toggleCalled).toBe(1)
-    expect(showProvidersCalled).toBe(1)
   })
 })
