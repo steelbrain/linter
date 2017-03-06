6df530844b3bcaed1d113b316a46736360274a31
Steel Brain <i@steelbrain.me>
2017-03-07 01:09:53 +0500

    :white_check_mark: Add specs for new commands behavior

--- spec/commands-spec.js
+++ spec/commands-spec.js
@@ -19,29 +19,45 @@ describe('Linter Commands', function() {
   it('properly notifies its listeners of command execution', async function() {
     let lintCalled = 0
     let debugCalled = 0
-    let toggleCalled = 0
+    let toggleActiveCalled = 0
+    const toggleLinterCalled = []
 
     commands.onShouldLint(() => ++lintCalled)
-    commands.onShouldToggleActiveEditor(() => ++toggleCalled)
+    commands.onShouldToggleActiveEditor(() => ++toggleActiveCalled)
     commands.onShouldDebug(() => ++debugCalled)
+    commands.onShouldToggleLinter(type => toggleLinterCalled.push(type))
 
     await atom.workspace.open(Path.join(__dirname, 'fixtures', 'file.txt'))
     const textEditor = atom.views.getView(atom.workspace.getActiveTextEditor())
 
     expect(lintCalled).toBe(0)
     expect(debugCalled).toBe(0)
-    expect(toggleCalled).toBe(0)
+    expect(toggleActiveCalled).toBe(0)
+    expect(toggleLinterCalled).toEqual([])
     atom.commands.dispatch(textEditor, 'linter:lint')
     expect(lintCalled).toBe(1)
     expect(debugCalled).toBe(0)
-    expect(toggleCalled).toBe(0)
+    expect(toggleActiveCalled).toBe(0)
+    expect(toggleLinterCalled).toEqual([])
     atom.commands.dispatch(textEditor, 'linter:toggle-active-editor')
     expect(lintCalled).toBe(1)
-    expect(toggleCalled).toBe(1)
+    expect(toggleActiveCalled).toBe(1)
     expect(debugCalled).toBe(0)
+    expect(toggleLinterCalled).toEqual([])
     atom.commands.dispatch(textEditor, 'linter:debug')
     expect(lintCalled).toBe(1)
     expect(debugCalled).toBe(1)
-    expect(toggleCalled).toBe(1)
+    expect(toggleActiveCalled).toBe(1)
+    expect(toggleLinterCalled).toEqual([])
+    atom.commands.dispatch(textEditor, 'linter:enable-linter')
+    expect(lintCalled).toBe(1)
+    expect(debugCalled).toBe(1)
+    expect(toggleActiveCalled).toBe(1)
+    expect(toggleLinterCalled).toEqual(['enable'])
+    atom.commands.dispatch(textEditor, 'linter:disable-linter')
+    expect(lintCalled).toBe(1)
+    expect(debugCalled).toBe(1)
+    expect(toggleActiveCalled).toBe(1)
+    expect(toggleLinterCalled).toEqual(['enable', 'disable'])
   })
 })
