592b06d0f190f917443abaf6b88343658e5e2bc8
Steel Brain <i@steelbrain.me>
2017-03-23 00:59:43 +0500

    :white_check_mark: Add specs for change

--- spec/common.js
+++ spec/common.js
@@ -18,12 +18,12 @@ export function getLinter(): Object {
     },
   }
 }
-export function getMessage(param: ?(boolean | string)): Object {
+export function getMessage(filePathOrNormalized: ?(boolean | string)): Object {
   const message: Object = { severity: 'error', excerpt: String(Math.random()), location: { file: __filename, position: [[0, 0], [0, 0]] } }
-  if (typeof param === 'boolean' && param) {
+  if (typeof filePathOrNormalized === 'boolean' && filePathOrNormalized) {
     normalizeMessages('Some Linter', [message])
-  } else if (typeof param === 'string') {
-    message.location.file = param
+  } else if (typeof filePathOrNormalized === 'string') {
+    message.location.file = filePathOrNormalized
   }
   return message
 }
