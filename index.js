e41697ebd7d4bf635777217ef8d78fc47e2fd10a
Steel Brain <i@steelbrain.me>
2017-04-12 23:01:02 +0500

    :new: Add interface for easy profiling

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 /* @flow */
 
+const profileAtomLinter = false
+
 if (atom.inDevMode()) {
   module.exports = require('./lib')
+} else if (profileAtomLinter) {
+  console.profile('linter load')
+  const mainModule = require('./lib.bundle.js')
+  console.profileEnd('linter load')
+  module.exports = Object.assign({}, mainModule, {
+    activate() {
+      console.profile('linter activate')
+      mainModule.activate.call(this)
+      console.profileEnd('linter activate')
+    },
+  })
 } else {
   module.exports = require('./lib.bundle.js')
 }
