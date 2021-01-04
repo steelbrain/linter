cf0ccef21049954d1f9e3fc6956366abbebcb39f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-04 14:20:41 -0600

    chore: add windows requestIdleCallback types

--- lib/types.ts
+++ lib/types.ts
@@ -78,3 +78,24 @@ declare module 'atom' {
     disposed: boolean
   }
 }
+
+
+// windows requestIdleCallback types
+export type RequestIdleCallbackHandle = any
+type RequestIdleCallbackOptions = {
+  timeout: number
+}
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean
+  timeRemaining: () => number
+}
+
+declare global {
+  interface Window {
+    requestIdleCallback: (
+      callback: (deadline: RequestIdleCallbackDeadline) => void,
+      opts?: RequestIdleCallbackOptions,
+    ) => RequestIdleCallbackHandle
+    cancelIdleCallback: (handle: RequestIdleCallbackHandle) => void
+  }
+}
