ba3fbaf9d7b741e3432a05bca2d6178169f36cd4
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-21 02:04:32 -0500

    chore: add built files

--- dist/types/linter-ui-default/index.d.ts
+++
@@ -1,8 +0,0 @@
-export var __esModule: boolean;
-declare function p(): void;
-declare function g(): void;
-declare function m(): any;
-declare function v(): any[];
-declare function y(e: any): void;
-declare function w(e: any): void;
-export { p as activate, g as deactivate, m as provideUI, v as provideIntentions, y as consumeSignal, w as consumeStatusBar };
